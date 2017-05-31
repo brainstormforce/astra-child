e2393290a09f93d0c6387d5b76c6cc13bda6f04a
Rushi Jagani <rushi.jagani@gmail.com>
2017-05-31 15:03:49 +0530

    Define astra child theme variable

--- functions.php
+++ functions.php
@@ -1,8 +1,24 @@
 <?php
+/**
+ * Astra Child Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Astra-child
+ * @since 1.0.0
+ */
 
+/**
+ * Define Constants
+ */
+define( 'ASTRA_CHILD_THEME_VERSION', '1.0.1' );
+
+/**
+ * Enqueue styles
+ */
 function child_enqueue_styles() {
 
-	wp_enqueue_style( 'astra-child-style', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), ASTRA_THEME_VERSION, 'all' );
+	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), ASTRA_CHILD_THEME_VERSION, 'all' );
 
 }
 
